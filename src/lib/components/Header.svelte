ea661115970dc97e6f4cd1461307bb090bbb7c7d
lain <livinglaincore@protonmail.com>
2024-09-25 17:11:33 -0400

    Add PGP keys. add about page

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,6 +4,9 @@
 	<nav>
 		<ul>
 			<li>
+				<a href="/about">About</a>
+			</li>
+			<li>
 				<a href="/blog">Blog</a>
 			</li>
 		</ul>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,13 @@
+<div>
+    I'm an embedded software developer (mainly), and I love all things tech!
+    <br>
+
+    <h2>Contact</h2>
+    You can contact me via:
+    <br>
+
+    <h3>Email</h3>
+    puppy (at) puppygirl dot tech (preferred) | <a href="/keys/puppy.public.asc">PGP</a><br>
+    livinglaincore (at) protonmail dot com | <a href="/keys/lain.public.asc">PGP</a>
+</div>
+
